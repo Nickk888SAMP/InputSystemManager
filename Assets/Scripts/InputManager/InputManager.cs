d9d0f00fd0b218ef20f33706170b5238eef5474a
Nickk888 <35627647+Nickk888SAMP@users.noreply.github.com>
2023-02-15 15:59:05 +0100

    Update

--- Assets/Scripts/InputManager/InputManager.cs
+++ Assets/Scripts/InputManager/InputManager.cs
@@ -37,16 +37,17 @@ public class InputManager : MonoBehaviour
         }
     }
 
-    private void Update() 
-    {
-        GetMouseScreenPosition();
-    }
-
     #endregion
 
     #region Public Methods
 
-    
+    /// <summary>
+    /// Returns a Vector2 value in the Out parameter providing the reference of the InputAction.
+    /// Returns a boolean if the Action was found or not. 
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public bool GetInputActionValue(InputActionReference reference, out Vector2 value)
     {
         if(GetAction(reference, out InputAction inputAction))
@@ -57,11 +58,25 @@ public class InputManager : MonoBehaviour
         value = Vector2.zero;
         return false;
     }
+    /// <summary>
+    /// Returns a Vector2 value in the Out parameter providing the name of the InputAction.
+    /// Returns a boolean if the Action was found or not.
+    /// </summary>
+    /// <param name="inputActionName"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public bool GetInputActionValue(string inputActionName, out Vector2 value)
     {
         return GetInputActionValue(GetInputReferenceByName(inputActionName), out value);
     }
 
+    /// <summary>
+    /// Returns a boolean value of the InputAction state providing the reference of the InputAction.
+    /// Will return false if the InputAction wasn't found.
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <param name="inputActionState"></param>
+    /// <returns></returns>
     public bool GetInputActionValue(InputActionReference reference, InputActionState inputActionState)
     {
         if(GetAction(reference, out InputAction inputAction))
@@ -75,25 +90,49 @@ public class InputManager : MonoBehaviour
                 default: return inputAction.WasPerformedThisFrame();
             }
         }
-        return inputAction.WasPerformedThisFrame();
+        return false;
     }
+
+    /// <summary>
+    /// Returns a boolean value of the InputAction state providing the name of the InputAction.
+    /// Will return false if the InputAction wasn't found.
+    /// </summary>
+    /// <param name="inputActionName"></param>
+    /// <param name="inputActionState"></param>
+    /// <returns></returns>
     public bool GetInputActionValue(string inputActionName, InputActionState inputActionState)
     {
         return GetInputActionValue(GetInputReferenceByName(inputActionName), inputActionState);
     }
 
+    /// <summary>
+    /// Returns the InputAction by providing the reference of the InputAction.
+    /// Will return "null" if the InputAction wasn't found.
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <returns></returns>
     public InputAction GetInputAction(InputActionReference reference)
     {
         GetAction(reference, out InputAction inputAction);
         return inputAction;
     }
 
+    /// <summary>
+    /// Return the InputAction by providing the name of the InputAction.
+    /// Will return "null" if the InputAction wasn't found.
+    /// </summary>
+    /// <param name="inputActionName"></param>
+    /// <returns></returns>
     public InputAction GetInputAction(string inputActionName)
     {
         GetAction(GetInputReferenceByName(inputActionName), out InputAction inputAction);
         return inputAction;
     }
 
+    /// <summary>
+    /// Returns a Vector2 with the X and Y position of the Mouse on the Screen.
+    /// </summary>
+    /// <returns></returns>
     public Vector2 GetMouseScreenPosition()
     {
         return InputSystem.GetDevice<Mouse>().position.ReadValue();
@@ -103,22 +142,46 @@ public class InputManager : MonoBehaviour
 
     #region Local Methods
 
+    /// <summary>
+    /// Initializes the action by finding the InputAction by name in the Input Action Asset.
+    /// </summary>
+    /// <param name="actionName"></param>
+    /// <param name="inputAction"></param>
     private void InitializeAction(string actionName, out InputAction inputAction)
     {
         inputAction = inputActionAsset.FindAction(actionName, true);
         inputAction.Enable();
     }
     
+    /// <summary>
+    /// Returns the InputActionReference providing the name of the InputAction.
+    /// Will return "null" if the reference wasn't found.
+    /// </summary>
+    /// <param name="inputActionName"></param>
+    /// <returns></returns>
     private InputActionReference GetInputReferenceByName(string inputActionName)
     {
         return inputActions.Where(x => x.Key.inputActionName == inputActionName).Select(x => x.Key).FirstOrDefault();
     }
 
+    /// <summary>
+    /// Returns the InputAction providing the name of the InputAction
+    /// Will return "null" if the reference wasn't found.
+    /// </summary>
+    /// <param name="inputActionName"></param>
+    /// <returns></returns>
     private InputAction GetInputActionByName(string inputActionName)
     {
         return inputActions.Where(x => x.Key.inputActionName == inputActionName).Select(x => x.Value).FirstOrDefault();
     }
     
+    /// <summary>
+    /// Returns the InputAction in the "out" parameter by providing the reference.
+    /// Will return "null" if the reference wasn't found.
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <param name="inputAction"></param>
+    /// <returns></returns>
     private bool GetAction(InputActionReference reference, out InputAction inputAction)
     {
         if(inputActions.TryGetValue(reference, out InputAction action))
--- Assets/Scripts/InputManagerCameraControllerTest.cs
+++ Assets/Scripts/InputManagerCameraControllerTest.cs
@@ -8,6 +8,8 @@ public class InputManagerCameraControllerTest : MonoBehaviour
     public InputActionReference moveActionReference;
     public InputActionReference sprintActionReference;
     public InputActionReference aimActionReference;
+    public InputActionReference shootActionReference;
+    public Transform bulletPrefab;
     public Transform cameraTransform;
     private float yRot, xRot;
     private float sprintMultiplier = 2.0f;
@@ -16,6 +18,7 @@ public class InputManagerCameraControllerTest : MonoBehaviour
     {
         bool isSprinting = InputManager.Instance.GetInputActionValue(sprintActionReference, InputActionState.IsPressed);
         bool isAiming = InputManager.Instance.GetInputActionValue(aimActionReference, InputActionState.IsPressed);
+        bool isShooting = InputManager.Instance.GetInputActionValue(shootActionReference, InputActionState.WasPressedThisFrame);
         if(InputManager.Instance.GetInputActionValue(lookActionReference, out Vector2 lookInputValue) && isAiming)
         {
             yRot = Mathf.Clamp(yRot += lookInputValue.y, -90, 90);
@@ -27,7 +30,13 @@ public class InputManagerCameraControllerTest : MonoBehaviour
             Vector3 translation = new Vector3(moveInputValue.x, 0, moveInputValue.y) * 100 * (isSprinting ? 5 : 1);
             cameraTransform.Translate(translation * Time.deltaTime, Space.Self);
         }
-
-        Debug.Log(InputManager.Instance.GetMouseScreenPosition());
+        if(isShooting)
+        {
+            Transform bullet = Instantiate(bulletPrefab, cameraTransform.position, Quaternion.identity);
+            if(bullet.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb.AddForce(cameraTransform.forward * 2000f);
+            }
+        }
     }
 }
