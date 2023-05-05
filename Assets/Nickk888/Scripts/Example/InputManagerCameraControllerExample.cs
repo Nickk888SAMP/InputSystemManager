46652d88595b17d2fe24e7227155ee2cf4d717f2
Nickk888 <35627647+Nickk888SAMP@users.noreply.github.com>
2023-05-05 23:52:17 +0200

    Reorganized files

---
+++ Assets/Nickk888/Scripts/Example/InputManagerCameraControllerExample.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManagerCameraControllerExample : MonoBehaviour 
+{
+    public InputActionReference lookActionReference;
+    public InputActionReference moveActionReference;
+    public InputActionReference sprintActionReference;
+    public InputActionReference aimActionReference;
+    public InputActionReference shootActionReference;
+
+    public Transform bulletPrefab;
+    public Transform cameraTransform;
+
+    private float yRot, xRot;
+    private float sprintMultiplier = 2.0f;
+
+    private void Update() 
+    {
+        bool isSprinting = InputManager.Instance.GetInputActionValue(sprintActionReference, InputActionState.IsPressed);
+        bool isAiming = InputManager.Instance.GetInputActionValue(aimActionReference, InputActionState.IsPressed);
+        bool isShooting = InputManager.Instance.GetInputActionValue(shootActionReference, InputActionState.WasPressedThisFrame);
+        
+        if(InputManager.Instance.GetInputActionValue(lookActionReference, out Vector2 lookInputValue) && isAiming)
+        {
+            yRot = Mathf.Clamp(yRot += lookInputValue.y, -90, 90);
+            xRot += lookInputValue.x;
+            cameraTransform.rotation = Quaternion.Euler(-yRot, xRot, 0);
+        }
+
+        if(InputManager.Instance.GetInputActionValue(moveActionReference, out Vector2 moveInputValue))
+        {
+            Vector3 translation = new Vector3(moveInputValue.x, 0, moveInputValue.y) * 100 * (isSprinting ? 5 : 1);
+            cameraTransform.Translate(translation * Time.deltaTime, Space.Self);
+        }
+
+        if(isShooting)
+        {
+            Transform bullet = Instantiate(bulletPrefab, cameraTransform.position, Quaternion.identity);
+            if(bullet.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb.AddForce(cameraTransform.forward * 2000f);
+            }
+        }
+    }
+}
